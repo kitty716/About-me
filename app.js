29a6cc1df39af3f752622c8bd3f2d454a9f69e4e
Jennifer Liu <jenniferliu716@gmail.com>
2016-05-10 14:32:36 -0700

    Skeleton of HTML + Javascript for first response

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//comment
+var userName = prompt('Hi! Thanks for visiting my About Me page !!! What is your name?');
+alert('Nice to meet you, ' + userName + '. I\'m going to ask you some questions about me. No pressure. Really. Make your best guess.');
+console.log('The user\'s name is ' + userName);
+
+var response1 = prompt('Do I have 3 cats?').toLowerCase();
+var answer1 = 'yes';
+
+if (response1 === answer1 || response1 === answer1[0]) {
+  alert('You are correct, ' + userName + '! My cats are Buddy, Alistair, and Trillian');
+} else {
+  alert('Nope, I really do have 3 cats.');
+}
